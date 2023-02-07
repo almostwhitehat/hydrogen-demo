c7ed327683bb5692d96d2112f797cdfb7a2f57b5
Matt Metzger <matthew.metzger@shopify.com>
2023-02-07 12:01:23 -0500

    Finished Begin development

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <section className="p-6 md:p-8 lg:p-12">
+      <h1 className="font-extrabold mb-4 text-5xl md:text-7xl">Hello World!</h1>
+      <p className="font-bold mb-3">Welcome to Hydrogen.</p>
+      <p>Hydrogen is a front-end framework.</p>
+    </section>
+  );
 }

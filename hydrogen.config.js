b8ba7f328c0d57df576811883af742403eaabe2b
Matt Metzger <matthew.metzger@shopify.com>
2023-02-09 12:25:23 -0500

    Rename session variables

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from "@shopify/hydrogen/config";
 export default defineConfig({
   shopify: {
     storeDomain:
-      process.env.HYDROGEN_HOSTNAME || "hydrogen-preview.myshopify.com",
+      process.env.PUBLIC_STORE_DOMAIN || "hydrogen-preview.myshopify.com",
     storefrontToken:
-      process.env.HYDROGEN_API_KEY || "3b580e70970c4528da70c98e097c2fa0",
+      process.env.PUBLIC_STOREFRONT_API_TOKEN ||
+      "3b580e70970c4528da70c98e097c2fa0",
     storefrontApiVersion: "2022-07",
   },
 });

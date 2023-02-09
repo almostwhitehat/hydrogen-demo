b514aa551e793d02e9f305140becdaeb31c95a2b
Matt Metzger <matthew.metzger@shopify.com>
2023-02-09 12:07:34 -0500

    Pull keys from env vars

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,9 +1,11 @@
-import {defineConfig} from '@shopify/hydrogen/config';
+import { defineConfig } from "@shopify/hydrogen/config";
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-    storefrontApiVersion: '2022-07',
+    storeDomain:
+      process.env.HYDROGEN_HOSTNAME || "hydrogen-preview.myshopify.com",
+    storefrontToken:
+      process.env.HYDROGEN_API_KEY || "3b580e70970c4528da70c98e097c2fa0",
+    storefrontApiVersion: "2022-07",
   },
 });
